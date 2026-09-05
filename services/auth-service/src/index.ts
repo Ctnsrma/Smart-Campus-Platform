82f7e8d4f857d1c3dc1892c84f04bff5e90e0c3b
Ctnsrma <chetansharma98004@gmail.com>
2026-09-05 13:05:38 +0530

    feat(auth-service): add Express server with GET /health endpoint

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Auth Service is running well.....")
+import express, {Request,Response} from "express";
+
+
+const app = express();
+const PORT = 3001;
+
+app.get('/health',(_req: Request,res: Response)=>{
+    res.status(200).json({
+        status: "OK",
+        service: "auth-service",
+        timestamp: new Date().toISOString(), 
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`[auth-service] listening on port ${PORT}`);
+});
